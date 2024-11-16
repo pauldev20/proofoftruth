986943d4c68e7a04751ce308b4ab911dbc0ab593
lorbke <lorbke@student.42heilbronn.de>
2024-11-16 18:59:05 +0700

    working voting logic

--- packages/foundry/contracts/HumanOracle.sol
+++ packages/foundry/contracts/HumanOracle.sol
@@ -2,7 +2,7 @@
 pragma solidity >=0.8.0 <0.9.0;
 
 // Useful for debugging. Remove when deploying to a live network.
-// import "forge-std/console.sol";
+import "forge-std/console.sol";
 
 contract HumanOracle {
 	// ====================
@@ -20,7 +20,7 @@ contract HumanOracle {
 	}
 
 	struct Stake {
-		Option[] options;
+		Option[] answers;
 		uint256  totalStake;
 	}
 
@@ -30,13 +30,13 @@ contract HumanOracle {
 		string[] answers;
 		uint256  startBlock;
 		uint256  durationInBlocks;
-		Stake    stake;
 	}
 
 	// ====================
 	// ==== Variables =====
 	// ====================
 
+	mapping (uint256 => Stake) public stakesForVoteIds;
 	mapping (address => User) public users;
 	Vote[] votes;
 
@@ -61,6 +61,43 @@ contract HumanOracle {
 		_;
 	}
 
+	modifier hasNotVoted(uint256 voteId) {
+		uint256 answerCount = getStakeAnswerCount(voteId);
+		address userAddr = address(msg.sender);
+		for (uint i = 0; i < answerCount; i++) {
+			bool hasVoted = hasUserVotedForStakeAnswer(userAddr, voteId, i);
+			if (hasVoted == true) {
+				revert("user already voted");
+			}
+		}
+		_;
+	}
+
+	modifier voteActive(uint256 voteId) {
+		require(getVoteStartBlock(voteId) <= block.number, "vote has not started");
+		require(getVoteStartBlock(voteId) + getVoteDurationInBlocks(voteId) >= block.number, "vote has ended");
+		_;
+	}
+
+	modifier voteEnded(uint256 voteId) {
+		require(getVoteStartBlock(voteId) + getVoteDurationInBlocks(voteId) < block.number, "vote still active");
+		_;
+	}
+
+	modifier userExists() {
+		if (users[address(msg.sender)].nullifierHash == 0) {
+			revert("user not existing");
+		}
+		_;
+	}
+
+	modifier userOldEnough(uint256 voteId) {
+		if (users[address(msg.sender)].createdAtBlock > getVoteStartBlock(voteId)) {
+			revert("user was created after voting begun");
+		}
+		_;
+	}
+
 	// ====================
 	// === Constructor ====
 	// ====================
@@ -71,88 +108,180 @@ contract HumanOracle {
 
 	// external
 
-	function signUpWithWorldId(uint256 nullifierHash, uint256[8] calldata proof) onlyNewUser external {
+	function signUpWithWorldId(uint256 merkleRoot, uint256 nullifierHash, uint256[8] calldata proof) onlyNewUser() external {
 		address userAddr = address(msg.sender);
-		User memory user = User({
+		User memory newUser = User({
 			nullifierHash: nullifierHash,
 			createdAtBlock: block.number
 		});
-		users[userAddr] = user;
-		emit UserRegistered(userAddr, user.nullifierHash, user.createdAtBlock);
+		users[userAddr] = newUser;
+		emit UserRegistered(userAddr, users[userAddr].nullifierHash, users[userAddr].createdAtBlock);
 	}
 
-	function submitVotingDecisionWithStake(uint256 voteId, uint256 answerIndex, uint256 stake) external {
-		emit VoteSubmitted(address(msg.sender), voteId, answerIndex, stake);
+	function submitVotingDecisionWithStake(uint256 voteId, uint256 answerIndex, uint256 amount) userOldEnough(voteId) userExists() hasNotVoted(voteId) voteActive(voteId) external {
+		require(amount <= 5, "max staking amount is 5");
+		address userAddr = address(msg.sender);
+		stakeForAnswer(userAddr, voteId, answerIndex, amount);
+		emit VoteSubmitted(userAddr, voteId, answerIndex, amount);
 	}
 
-	function claimRewardForVote(uint256 voteId) external {
-		emit RewardClaimed(address(msg.sender), voteId, 10);
+	function claimRewardForVote(uint256 voteId) voteEnded(voteId) external returns (uint256) {
+		address userAddr = address(msg.sender);
+		uint256 payout = getStakeResolvedUserAmount(userAddr, voteId);
+		emit RewardClaimed(userAddr, voteId, payout);
+		return payout;
 	}
 
-	function isUserRegistered(address userAddr) external pure returns (bool) {
-		return true;
+	function isUserRegistered(address userAddr) external view returns (bool) {
+		if (users[userAddr].nullifierHash != 0) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 	function createVote(string calldata question, string[] calldata answers, uint256 startBlock, uint256 durationInBlocks) external {
-		emit VoteCreated(0, question, startBlock, durationInBlocks);
+		uint256 voteId = votes.length;
+		Vote memory newVote = Vote({
+			id: voteId,
+			question: question,
+			answers: answers,
+			startBlock: startBlock,
+			durationInBlocks: durationInBlocks
+		});
+		votes.push(newVote);
+
+		createNewStake(voteId);
+
+		emit VoteCreated(votes[voteId].id, votes[voteId].question, votes[voteId].startBlock, votes[voteId].durationInBlocks);
 	}
 
-	function getVotingPage(uint256 voteId) external pure returns (
+	function getVotingPage(uint256 voteId) external view returns (
 		string memory question,
 		string[] memory answers,
 		uint256 totalStake,
 		uint256[] memory stakePerAnswer
 	) {
-		// Mock Data based on voteId
-		if (voteId == 1) {
-			question = "What is your favorite blockchain?";
-			answers = new string[](3);
-			answers[0] = "Ethereum";
-			answers[1] = "Binance Smart Chain";
-			answers[2] = "Polkadot";	
-			totalStake = 1000;	
-			stakePerAnswer = new uint256[](3);
-			stakePerAnswer[0] = 600; // Ethereum
-			stakePerAnswer[1] = 300; // Binance Smart Chain
-			stakePerAnswer[2] = 100; // Polkadot
+		uint256 answerCount = getStakeAnswerCount(voteId);
+		totalStake = getStakeTotalStake(voteId);
+		stakePerAnswer = new uint256[](answerCount);
+
+		for (uint i = 0; i < answerCount; i++) {
+			stakePerAnswer[i] = getStakeAnswerStake(voteId, i);
 		}
-		else if (voteId == 2) {
-			question = "Which DeFi project do you trust the most?";
-			answers = new string[](2);
-			answers[0] = "Uniswap";
-			answers[1] = "Aave";	
-			totalStake = 500;	
-			stakePerAnswer = new uint256[](2);
-			stakePerAnswer[0] = 350;
-			stakePerAnswer[1] = 150;
-		}
-		else {
-			question = new string(0);
-			answers = new string[](0);
-			totalStake = 0;
-			stakePerAnswer = new uint256[](0);
-		}
-		return (question, answers, totalStake, stakePerAnswer);
+
+		return (getVoteQuestion(voteId), getVoteAnswers(voteId), totalStake, stakePerAnswer);
 	}	
 
-	function getVotingList() external pure returns (
+	function getVotingList() external view returns (
 		uint256[] memory ids,
 		string[] memory questions,
 		uint256[] memory totalStakes
 	) {
-		uint256 numberOfVotes = 2;	
-		ids = new uint256[](numberOfVotes);
-		questions = new string[](numberOfVotes);
-		totalStakes = new uint256[](numberOfVotes);	
+		uint256 voteCount = getVoteCount();
+		ids = new uint256[](voteCount);
+		questions = new string[](voteCount);
+		totalStakes = new uint256[](voteCount);	
 
-		ids[0] = 1;
-		questions[0] = "What is your favorite blockchain?";
-		totalStakes[0] = 1000;	
-
-		ids[1] = 2;
-		questions[1] = "Which DeFi project do you trust the most?";
-		totalStakes[1] = 500;
+		for (uint i = 0; i < voteCount; i++) {
+			ids[i] = i;
+			questions[i] = getVoteQuestion(i);
+			totalStakes[i] = getStakeTotalStake(i);
+		}
 
 		return (ids, questions, totalStakes);
+	}
+
+	// internal
+
+	// stake related
+	function createNewStake(uint256 voteId) internal {
+		Stake storage newStake = stakesForVoteIds[voteId];
+		uint256 answerCount = votes[voteId].answers.length;
+		for (uint i = 0; i < answerCount; i++) {
+			newStake.answers.push();
+		}
+	}
+
+	function stakeForAnswer(address userAddr, uint256 voteId, uint256 answerIndex, uint256 amount) internal {
+		stakesForVoteIds[voteId].answers[answerIndex].userStakes[userAddr] = amount;
+		stakesForVoteIds[voteId].answers[answerIndex].totalStake += amount;
+		stakesForVoteIds[voteId].totalStake += amount;
+	}
+
+	function getStakeResolvedUserAmount(address userAddr, uint256 voteId) internal view returns (uint256 amount) {
+		uint256 highestStakeAnswerIndex = getStakeHighestAnswerIndex(voteId);
+		if (!hasUserVotedForStakeAnswer(userAddr, voteId, highestStakeAnswerIndex)) {
+			return 0;
+			// revert("user has not won");
+		}
+		uint256 userStake = getUserStakeOfStakeAnswer(userAddr, voteId, highestStakeAnswerIndex);
+		console.log(userStake, "getUserStakeOfStakeAnswer");
+		uint256 totalStake = getStakeTotalStake(voteId);
+		console.log(totalStake, "getStakeTotalStake");
+		uint256 answerStake = getStakeAnswerStake(voteId, highestStakeAnswerIndex);
+		console.log(answerStake, "getStakeAnswerStake");
+		uint256 userPayout = totalStake / answerStake * userStake;
+		return userPayout;
+	}
+
+	function getStakeHighestAnswerIndex(uint256 voteId) internal view returns (uint256 answerIndex) {
+		uint256 answerCount = getStakeAnswerCount(voteId);
+		uint256 highestAmount = 0;
+		uint256 highestIndex = 0;
+
+		for (uint i = 0; i < answerCount; i++) {
+			uint256 answerStake = getStakeAnswerStake(voteId, i);
+			if (highestAmount < answerStake) {
+				highestAmount = answerStake;
+				highestIndex = i;
+			}
+		}
+		return highestIndex;
+	}
+
+	function getStakeAnswerStake(uint256 voteId, uint256 answerIndex) internal view returns (uint256 stake) {
+		return stakesForVoteIds[voteId].answers[answerIndex].totalStake;
+	}
+
+	function getStakeAnswerCount(uint256 voteId) internal view returns (uint256 count) {
+		return stakesForVoteIds[voteId].answers.length;
+	}
+
+	function getStakeTotalStake(uint256 voteId) internal view returns (uint256 stake) {
+		return stakesForVoteIds[voteId].totalStake;
+	}
+
+	function hasUserVotedForStakeAnswer(address userAddr, uint256 voteId, uint256 answerIndex) internal view returns (bool voted) {
+		if (stakesForVoteIds[voteId].answers[answerIndex].userStakes[userAddr] != 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	function getUserStakeOfStakeAnswer(address userAddr, uint256 voteId, uint256 answerIndex) internal view returns (uint256 amount) {
+		return stakesForVoteIds[voteId].answers[answerIndex].userStakes[userAddr];
+	}
+
+	// vote related
+	function getVoteCount() internal view returns (uint256 count) {
+		return votes.length;
+	}
+
+	function getVoteQuestion(uint256 voteId) internal view returns (string memory question) {
+		return votes[voteId].question;
+	}
+
+	function getVoteAnswers(uint256 voteId) internal view returns (string[] memory answers) {
+		return votes[voteId].answers;
+	}
+
+	function getVoteStartBlock(uint256 voteId) internal view returns (uint256 startBlock) {
+		return votes[voteId].startBlock;
+	}
+
+	function getVoteDurationInBlocks(uint256 voteId) internal view returns (uint256 durationInBlocks) {
+		return votes[voteId].durationInBlocks;
 	}
 }
